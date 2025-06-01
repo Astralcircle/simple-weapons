7da91bc1f228a5793381ac3e6e739784843a5f03
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-06-01 04:58:19 +0300

    Remove addon support

--- lua/weapons/simple_css_usp.lua
+++ lua/weapons/simple_css_usp.lua
@@ -75,23 +75,9 @@ function SWEP:AltFire()
 	self:SetSuppressed(suppressed)
 	self:SendTranslatedWeaponAnim(suppressed and ACT_VM_ATTACH_SILENCER or ACT_VM_DETACH_SILENCER)
 
-	-- Dynamic Weapon Reverb support
-	self.dwr_customIsSuppressed = self:GetSuppressed()
-
-	if game.SinglePlayer() then
-		self:CallOnClient("UpdateReverb", tostring(suppressed))
-	end
-
 	local duration = CurTime() + self:SequenceDuration()
-
 	self:SetNextFire(duration)
 	self:SetNextIdle(duration)
-end
-
-if CLIENT then
-	function SWEP:UpdateReverb(bool)
-		self.dwr_customIsSuppressed = tobool(bool)
-	end
 end
 
 function SWEP:EmitFireSound()
