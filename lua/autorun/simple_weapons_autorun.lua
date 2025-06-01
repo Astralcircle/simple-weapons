87b776937ac051793e8dd736a086cf6af52c6efe
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-06-01 09:01:29 +0300

    Use cached values

--- lua/autorun/simple_weapons_autorun.lua
+++ lua/autorun/simple_weapons_autorun.lua
@@ -5,26 +5,26 @@ if SERVER then
 	CreateConVar("simple_weapons_range_mult", 1, FCVAR_ARCHIVE, "The range modifier to use for weapons.", 0)
 	CreateConVar("simple_weapons_recoil_mult", 1, FCVAR_ARCHIVE, "The recoil modifier to use for weapons.", 0)
 else
-	CreateClientConVar("simple_weapons_auto_reload", 1, true, true, "Whether weapons should automatically reload when you fire them.")
-	CreateClientConVar("simple_weapons_scopes", 1, true, false, "Whether to use scopes when zooming.")
-	CreateClientConVar("simple_weapons_swayscale", 1, true, false, "The amount of viewmodel sway to apply to weapons")
-	CreateClientConVar("simple_weapons_bobscale", 1, true, false, "The amount of viewmodel bob to apply to weapons")
-	CreateClientConVar("simple_weapons_vm_offset_x", 0, true, false, "The forward/back offset to use for viewmodels.")
-	CreateClientConVar("simple_weapons_vm_offset_y", 0, true, false, "The left/right offset to use for viewmodels.")
-	CreateClientConVar("simple_weapons_vm_offset_z", 0, true, false, "The up/down offset to use for viewmodelss.")
+	local simple_weapons_auto_reload = CreateClientConVar("simple_weapons_auto_reload", 1, true, true, "Whether weapons should automatically reload when you fire them.")
+	local simple_weapons_scopes = CreateClientConVar("simple_weapons_scopes", 1, true, false, "Whether to use scopes when zooming.")
+	local simple_weapons_swayscale = CreateClientConVar("simple_weapons_swayscale", 1, true, false, "The amount of viewmodel sway to apply to weapons")
+	local simple_weapons_bobscale = CreateClientConVar("simple_weapons_bobscale", 1, true, false, "The amount of viewmodel bob to apply to weapons")
+	local simple_weapons_vm_offset_x = CreateClientConVar("simple_weapons_vm_offset_x", 0, true, false, "The forward/back offset to use for viewmodels.")
+	local simple_weapons_vm_offset_y = CreateClientConVar("simple_weapons_vm_offset_y", 0, true, false, "The left/right offset to use for viewmodels.")
+	local simple_weapons_vm_offset_z = CreateClientConVar("simple_weapons_vm_offset_z", 0, true, false, "The up/down offset to use for viewmodelss.")
 
 	hook.Add("PopulateToolMenu", "simple_weapons", function()
 		spawnmenu.AddToolMenuOption("Utilities", "User", "simple_weapons_cl", "Simple Weapons", "", "", function(pnl)
-			pnl:Help("Configure you're weapons here")
+			pnl:Help("Configure your weapons here")
 
 			local convars = {
-				simple_weapons_auto_reload = GetConVar("simple_weapons_auto_reload"):GetDefault(),
-				simple_weapons_scopes = GetConVar("simple_weapons_scopes"):GetDefault(),
-				simple_weapons_swayscale = GetConVar("simple_weapons_swayscale"):GetDefault(),
-				simple_weapons_bobscale = GetConVar("simple_weapons_bobscale"):GetDefault(),
-				simple_weapons_vm_offset_x = GetConVar("simple_weapons_vm_offset_x"):GetDefault(),
-				simple_weapons_vm_offset_y = GetConVar("simple_weapons_vm_offset_y"):GetDefault(),
-				simple_weapons_vm_offset_z = GetConVar("simple_weapons_vm_offset_z"):GetDefault()
+				simple_weapons_auto_reload = simple_weapons_auto_reload:GetDefault(),
+				simple_weapons_scopes = simple_weapons_scopes:GetDefault(),
+				simple_weapons_swayscale = simple_weapons_swayscale:GetDefault(),
+				simple_weapons_bobscale = simple_weapons_bobscale:GetDefault(),
+				simple_weapons_vm_offset_x = simple_weapons_vm_offset_x:GetDefault(),
+				simple_weapons_vm_offset_y = simple_weapons_vm_offset_y:GetDefault(),
+				simple_weapons_vm_offset_z = simple_weapons_vm_offset_z:GetDefault()
 			}
 
 			pnl:ToolPresets("simple_weapons_cl", convars)
